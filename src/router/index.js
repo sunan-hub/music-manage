74a11503b52dfd9a8b031bb9f1790b400d555ff0
sunan <1816546887@qq.com>
2021-04-26 14:58:17 +0800

    feat: 第一次提交，完成所有功能

---
+++ src/router/index.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: resolve => require(['../pages/Login.vue'], resolve)
+    },
+
+    {
+      path: '/Home',
+      component: resolve => require(['../components/Home.vue'], resolve),
+      children: [
+        {
+          path: '/Info',
+          component: resolve => require(['../pages/InfoPage.vue'], resolve)
+        },
+        {
+          path: '/Consumer',
+          component: resolve => require(['../pages/ConsumerPage.vue'], resolve)
+        },
+        {
+          path: '/Singer',
+          component: resolve => require(['../pages/SingerPage.vue'], resolve)
+        },
+        {
+          path: '/SongList',
+          component: resolve => require(['../pages/SongListPage.vue'], resolve)
+        },
+        {
+          path: '/Song',
+          component: resolve => require(['../pages/SongPage.vue'], resolve)
+        },
+        {
+          path: '/ListSong',
+          component: resolve => require(['../pages/ListSongPage.vue'], resolve)
+        },
+        {
+          path: '/Collect',
+          component: resolve => require(['../pages/CollectPage.vue'], resolve)
+        },
+        {
+          path: '/Comment',
+          component: resolve => require(['../pages/CommentPage.vue'], resolve)
+        }
+      ]
+    }
+  ]
+})
+
+// 重复点击导航时，控制台出现报错 ，虽然不影响功能使用，但也不能视而不见
+const VueRouterPush = Router.prototype.push
+Router.prototype.push = function push(to) {
+  return VueRouterPush.call(this, to).catch(err => err)
+}
